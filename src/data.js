1a222b0c4821475cb93a906372ec2a3e58f074c4
Lisa Trevis <l_trevis@hotmail.com>
2021-02-09 13:40:17 -0800

    Added Work section and CSS

--- src/data.js
+++ src/data.js
@@ -125,4 +125,54 @@ export const people = [
   },
 ];
 
+export const jobs = [
+  {
+    id: 1,
+    title: "Programmer Analyst",
+    company: "CEB",
+    dates: "June 2018 - Dec 2020",
+    duties: [
+      "Planned and managed database content transition to new platforms / applications with multiple technical and non-technical teams.",
+      "Designed internal application using Figma. Modified externally sourced content to match internal content using HTML & CSS.",
+      "Created Python scripts which scraped websites and modified data to match required internal format.",
+      "Trained non-technical attorney users on a suite of new applications (Litmos, Panopto, SalesForce, SmartSheet, Contentful, and Intercom). Created internal documentation and user guides. Provided friendly ongoing IT support.",
+      "Managed content pipeline from uploads through publication to website.",
+    ],
+  },
+  {
+    id: 2,
+    title: "Instructor",
+    company: "Codify Academy",
+    dates: "April 2017 - Sept 2018",
+    duties: [
+      "Taught Front End Web Development to students new to programming via a 16 week in-person course.",
+      "Topics covered included HTML5, CSS3, JavaScript, jQuery, Bootstrap, APIs, JSON, AJAX, and frameworks like VUE, React, and Angular.",
+      "My cohorts consistently had the highest graduation rate of any instructor there.",
+    ],
+  },
+  {
+    id: 3,
+    title: "Manager, National Accounts",
+    company: "MultiPlan, Inc.",
+    dates: "Sept 2011 - Sept 2014",
+    duties: [
+      "Managed and grew revenue on multi-million dollar national accounts.",
+      "Increased revenue streams by up-selling and cross-selling products and market segments.",
+      "Managed implementation of new Clients and ongoing relationships between internal teams and Client teams for all technical, financial, and service issues.",
+    ],
+  },
+  {
+    id: 4,
+    title: "National Sales Manager",
+    company: "IHP, Inc.",
+    dates: "Oct 2008 - Aug 2011",
+    duties: [
+      "Negotiated multi-million dollar contracts from initial contact to final signature.",
+      "Exceeded double digit revenue goals every year.",
+      "Managed all aspects of the Client Accounts including financial, technical, and client relations.",
+      "Served as SalesForce Administrator.",
+    ],
+  },
+];
+
 export default sublinks;
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { jobs } from "../data";
 
 const Work = () => {
+  const [value, setValue] = useState(0);
+
+  const { title, company, dates, duties } = jobs[value];
   return (
     <div>
-      <h2>Experience</h2>
+      <section className="work-section">
+        <h2 className="title">Experience</h2>
+        <div className="center">
+          <div className="btn-container">
+            {jobs.map((job, index) => {
+              return (
+                <button
+                  key={job.id}
+                  onClick={() => setValue(index)}
+                  className={`job-btn ${index === value && "active-btn"}`}
+                >
+                  {job.company}
+                </button>
+              );
+            })}
+          </div>
+          <article className="job-info">
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties.map((duty, index) => {
+              return (
+                <div key={index} className="job-desc">
+                  <FaStar className="job-icon"></FaStar>
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </section>
     </div>
   );
 };
