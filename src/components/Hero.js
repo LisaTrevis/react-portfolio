56b45829a7898e0facabbe2d8eb7a10f589697b5
Lisa Trevis <l_trevis@hotmail.com>
2021-03-02 13:12:40 -0800

    Updated hero icon and CSS

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,46 +1,47 @@
-import React from "react";
-import PixelMe from "../images/Pixel_Me.png";
-import { useGlobalContext } from "../context";
-import { FaInstagram, FaTwitter, FaAngleRight } from "react-icons/fa";
+import React from 'react'
+import PixelMe from '../images/Pixel_Me.png'
+import { useGlobalContext } from '../context'
+import { FaInstagram, FaTwitter } from 'react-icons/fa'
+import { GiFallingStar } from 'react-icons/gi'
 
 const Hero = () => {
-  const { closeSubmenu } = useGlobalContext();
+  const { closeSubmenu } = useGlobalContext()
   return (
-    <section className="section hero" onMouseOver={closeSubmenu}>
-      <div className="hero-center">
-        <article className="hero-image">
-          <img src={PixelMe} className="headshot-img" alt="head shot" />
+    <section className='section hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-image'>
+          <img src={PixelMe} className='headshot-img' alt='head shot' />
         </article>
-        <article className="hero-info">
-          <div className="hero-tagline">
-            <FaAngleRight className="hero-icon" />
-            <h2>Web Developer</h2>
+        <article className='hero-info'>
+          <div className='hero-tagline'>
+            <GiFallingStar className='hero-icon' />
+            <h2>Software Engineer</h2>
           </div>
-          <div className="hero-tagline">
-            <FaAngleRight className="hero-icon" />
+          <div className='hero-tagline'>
+            <GiFallingStar className='hero-icon' />
             <h2>Problem Solver</h2>
           </div>
-          <div className="hero-tagline">
-            <FaAngleRight className="hero-icon" />
-            <h2>UI/UX Designer</h2>
+          <div className='hero-tagline'>
+            <GiFallingStar className='hero-icon' />
+            <h2>UI/UX Developer</h2>
           </div>
         </article>
       </div>
-      <div className="footer">
+      <div className='footer'>
         <p>Copyright &#169; 2021 Lisa Trevis.</p>
         <p>All rights reserved.</p>
-        <div className="pixel">
+        <div className='pixel'>
           <p>Logo pixel art by Glenn Brooks</p>
-          <a href="https://www.instagram.com/terderrer">
-            <FaInstagram className="icon" />
+          <a href='https://www.instagram.com/terderrer'>
+            <FaInstagram className='icon' />
           </a>
-          <a href="https://twitter.com/terderrer">
-            <FaTwitter className="icon" />
+          <a href='https://twitter.com/terderrer'>
+            <FaTwitter className='icon' />
           </a>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
